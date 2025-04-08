72774af737a6662f72080988440ba42dc73a61cd
nicholast013 <gamingnicholast2@gmail.com>
2025-04-08 15:14:49 +0700

    test1

---
+++ goofy.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById('canvaas')
+const button = document.getElementById('what')
+
+const jsConfetti = new JSConfetti({ canvas })
+
+setTimeout(() => {
+    jsConfetti.addConfetti()
+}, 500)
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti()
+})
+
+window.addEventListener("load", (event) => {
+    setInterval(, 5000);
+});
